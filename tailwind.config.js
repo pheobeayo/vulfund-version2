478530068b6e0f29a89180f4a1fc8cd7135dc460
pheobeayo <pheobeayo@gmail>
2024-11-16 12:19:48 +0100

    tabpanels

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
+
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+    "./src/**/*.{js,ts,jsx,tsx}",
+    
+  ],
   theme: {
     extend: {
       fontFamily: {
-        'opensans': ["Open Sans"," sans-serif"],
-        'titiliumweb': ["Titillium Web"," sans-serif"],
+        'opensans': ["Open Sans", " sans-serif"],
+        'titiliumweb': ["Titillium Web", " sans-serif"],
         'montserrat': ['Montserrat', 'sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
 
